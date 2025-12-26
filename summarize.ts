1fae8f020f392d7e7805a781c128feda8a4b697b
Jan Aagaard <jan@aagaard.net>
2025-12-26 22:37:45 +0100

    Extract content with Readability

--- summarize.ts
+++ summarize.ts
@@ -1,11 +1,35 @@
-const webpageUrl = "https://henrikleth.com/ironman/";
+import { JSDOM } from "jsdom";
+import { Readability } from "@mozilla/readability";
 
-const response = await fetch(webpageUrl);
-if (!response.ok) {
-  throw new Error(`Failed to fetch the webpage: ${response.statusText}.`);
+const url = "https://henrikleth.com/ironman/";
+
+async function main() {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch the webpage: ${response.statusText}.`);
+  }
+
+  const html = await response.text();
+
+  const dom = new JSDOM(html, { url: url });
+  const reader = new Readability(dom.window.document);
+  const article = reader.parse();
+
+  if (article === null) {
+    console.error("Readability could not extract an article from the page.");
+    console.log(html.substring(0, 1_000));
+    return;
+  }
+
+  console.log("Successfully extracted by Readability:\n");
+  console.log(article.title);
+  console.log();
+  console.log(article.textContent?.substring(0, 1_000));
 }
 
-const content = await response.text();
-console.log(content.substring(0, 2000));
+main().catch((err) => {
+  console.error(err);
+  process.exitCode = 1;
+});
 
 export {};
