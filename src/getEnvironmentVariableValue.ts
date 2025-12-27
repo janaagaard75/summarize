41575052b49a366ab70add326d39742d783041b7
Jan Aagaard <jan@aagaard.net>
2025-12-27 14:07:49 +0100

    Suppress log from dotenv

--- src/getEnvironmentVariableValue.ts
+++ src/getEnvironmentVariableValue.ts
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({
+  quiet: true,
+});
 
 export const getEnvironmentVariableValue = (variableName: string): string => {
   const value = process.env[variableName];
